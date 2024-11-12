bad31bb90eefd68a9bf9da055ce692838628de1b
Temitope Ajayi <bhh500@myy.haaga-helia.fi>
2024-11-12 23:56:01 +0200

    Add Vite configuration with API proxy settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi',
+        changeOrigin: true,
+      }
+    }
+  }
 })
